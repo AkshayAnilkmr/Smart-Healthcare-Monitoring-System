875d804f50c50c488fd01966033b6aca8a45df9b
Akshay Anilkumar <121080122+AkshayAnilkmr@users.noreply.github.com>
2025-06-12 13:45:39 +0530

    Create  config.h

---
+++ config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi credentials
+#define WIFI_SSID "your_SSID"
+#define WIFI_PASSWORD "your_PASSWORD"
+
+// Firebase project credentials
+#define FIREBASE_HOST "your-project.firebaseio.com"
+#define FIREBASE_AUTH "your_database_secret"
+
+#endif
